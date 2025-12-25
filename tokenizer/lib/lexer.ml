2bf54cc60dfe12646bc8b2297954a2460b3a3309
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-25 20:48:47 +0100

    Implement whitespace tokenizer with lowercase

--- tokenizer/lib/lexer.ml
+++ tokenizer/lib/lexer.ml
@@ -1,1 +1,6 @@
-let tokenize _text = []
+let tokenize text =
+  text
+  |> String.trim
+  |> String.split_on_char ' '
+  |> List.filter (fun s -> String.length s > 0)
+  |> List.map String.lowercase_ascii
