1f542714b7ff30811be2825e096489c4a13f3079
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-26 17:11:57 +0100

    feat: add potentially usefule constants

--- indexer/pkg/json/lexer.go
+++ indexer/pkg/json/lexer.go
@@ -1,3 +1,16 @@
 package json
 
 type TokenType int
+
+const (
+	TokenLBracket TokenType = iota // [
+	TokenRBracket                  // ]
+	TokenLBrace                    // {
+	TokenRBrace                    // }
+	TokenColon                     // :
+	TokenComma                     // ,
+	TokenString                    // "..."
+	TokenNumber                    // 123
+	TokenEOF
+	TokenError
+)
