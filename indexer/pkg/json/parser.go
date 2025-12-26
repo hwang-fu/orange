38daef13e2e9e5d7f726f4c77cd0adb38b006b4f
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-26 17:20:32 +0100

    feat: add expect()

--- indexer/pkg/json/parser.go
+++ indexer/pkg/json/parser.go
@@ -1,5 +1,7 @@
 package json
 
+import "errors"
+
 // TokenEntry represents a single token from the OCaml tokenizer.
 // Matches the JSON format: {"token": "word", "pos": 0}
 type TokenEntry struct {
@@ -15,4 +17,15 @@ type Parser struct {
 // advance moves to the next token from the lexer.
 func (p *Parser) advance() {
 	p.curr = p.lexer.Next()
+}
+
+// expect checks that the current token matches the expected type.
+// If it matches, it advances to the next token.
+// Returns an error if the token type doesn't match.
+func (p *Parser) expect(t TokenType) error {
+	if p.curr.Type != t {
+		return errors.New("unexpected token")
+	}
+	p.advance()
+	return nil
 }
