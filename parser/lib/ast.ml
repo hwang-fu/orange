bfd66233493203d9ab6fdea9f6b4d1b7d2a7223a
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-27 14:57:01 +0100

    feat: add to_json()

--- parser/lib/ast.ml
+++ parser/lib/ast.ml
@@ -13,3 +13,15 @@ let rec to_string = function
   | Or (l, r) -> Printf.sprintf "Or(%s, %s)" (to_string l) (to_string r)
   | Not e -> Printf.sprintf "Not(%s)" (to_string e)
 ;;
+
+(* Convert AST to JSON string *)
+let rec to_json = function
+  | Term w -> Printf.sprintf {|{"type": "term", "value": "%s"}|} w
+  | Phrase words ->
+    Printf.sprintf {|{"type": "phrase", "value": "%s"}|} (String.concat " " words)
+  | And (l, r) ->
+    Printf.sprintf {|{"type": "and", "left": %s, "right": %s}|} (to_json l) (to_json r)
+  | Or (l, r) ->
+    Printf.sprintf {|{"type": "or", "left": %s, "right": %s}|} (to_json l) (to_json r)
+  | Not e -> Printf.sprintf {|{"type": "not", "expr": %s}|} (to_json e)
+;;
