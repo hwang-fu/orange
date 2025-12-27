652f6b6047f8ab3e2b32f9d9c8ad0eace965f69c
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-27 14:56:52 +0100

    feat: add to_string()

--- parser/lib/ast.ml
+++ parser/lib/ast.ml
@@ -4,3 +4,12 @@ type expr =
   | And of expr * expr (* expr AND expr *)
   | Or of expr * expr (* expr OR expr *)
   | Not of expr (* NOT expr *)
+
+(* Convert AST to string for debugging *)
+let rec to_string = function
+  | Term w -> Printf.sprintf "Term(%s)" w
+  | Phrase words -> Printf.sprintf "Phrase(%s)" (String.concat " " words)
+  | And (l, r) -> Printf.sprintf "And(%s, %s)" (to_string l) (to_string r)
+  | Or (l, r) -> Printf.sprintf "Or(%s, %s)" (to_string l) (to_string r)
+  | Not e -> Printf.sprintf "Not(%s)" (to_string e)
+;;
