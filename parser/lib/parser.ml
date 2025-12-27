4cf19481445ce57c07bc00cf5eba5286d6e7d52f
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-27 15:00:19 +0100

    feat: add Parser.parse_phrase()

--- parser/lib/parser.ml
+++ parser/lib/parser.ml
@@ -24,3 +24,21 @@ let match_token parser token =
     true)
   else false
 ;;
+
+(* Parse a phrase: "word1 word2 ..." *)
+let parse_phrase parser =
+  let rec collect_words acc =
+    match parser.current with
+    | Token.WORD w ->
+      advance parser;
+      collect_words (w :: acc)
+    | Token.QUOTE ->
+      advance parser;
+      List.rev acc
+    | _ ->
+      (* Missing closing quote - return what we have *)
+      List.rev acc
+  in
+  let words = collect_words [] in
+  Ast.Phrase words
+;;
