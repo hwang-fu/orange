828f25257e8b88e9d4d28008edef6b088c1bf3d1
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-27 14:54:42 +0100

    feat: add advance()

--- parser/lib/lexer.ml
+++ parser/lib/lexer.ml
@@ -11,3 +11,10 @@ let is_at_end lexer = lexer.pos >= String.length lexer.input
 
 (* Peek at current character without consuming *)
 let peek lexer = if is_at_end lexer then None else Some lexer.input.[lexer.pos]
+
+(* Advance to next character and return current *)
+let advance lexer =
+  let c = lexer.input.[lexer.pos] in
+  lexer.pos <- lexer.pos + 1;
+  c
+;;
