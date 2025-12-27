6037e05a27a16dd17e92a925094fef4d10201b83
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-27 15:00:08 +0100

    feat: add Parser.match_token()

--- parser/lib/parser.ml
+++ parser/lib/parser.ml
@@ -15,3 +15,12 @@ let advance parser = parser.current <- Lexer.next_token parser.lexer
 
 (* Check if current token matches expected type *)
 let check parser token = parser.current = token
+
+(* Consume token if it matches, otherwise return false *)
+let match_token parser token =
+  if check parser token
+  then (
+    advance parser;
+    true)
+  else false
+;;
