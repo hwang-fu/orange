f1db9287f1f9a65d990ef51889bd82c0626119c8
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-27 14:54:27 +0100

    feat: add peek()

--- parser/lib/lexer.ml
+++ parser/lib/lexer.ml
@@ -8,3 +8,6 @@ let create input = { input; pos = 0 }
 
 (* Check if we've reached end of input *)
 let is_at_end lexer = lexer.pos >= String.length lexer.input
+
+(* Peek at current character without consuming *)
+let peek lexer = if is_at_end lexer then None else Some lexer.input.[lexer.pos]
