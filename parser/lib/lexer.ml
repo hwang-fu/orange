a87d07c29ab64e2aff2b5076a25fc6c03ac4748d
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-27 14:54:15 +0100

    feat: add is_at_end()

--- parser/lib/lexer.ml
+++ parser/lib/lexer.ml
@@ -5,3 +5,6 @@ type t =
 
 (* Create a new lexer from input string *)
 let create input = { input; pos = 0 }
+
+(* Check if we've reached end of input *)
+let is_at_end lexer = lexer.pos >= String.length lexer.input
