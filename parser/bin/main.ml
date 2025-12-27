deb44fa545332340b57c561ec6847ff900d00125
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-27 15:04:25 +0100

    feat: add query parser CLI entry point

--- parser/bin/main.ml
+++ parser/bin/main.ml
@@ -1,3 +1,5 @@
+(* main.ml - Query parser CLI *)
+
 let read_all () =
   let buf = Buffer.create 256 in
   try
@@ -7,4 +9,13 @@ let read_all () =
     assert false
   with
   | End_of_file -> Buffer.contents buf
+;;
+
+let () =
+  let input = read_all () |> String.trim in
+  if String.length input = 0
+  then print_endline {|{"error": "empty input"}|}
+  else (
+    let ast = Parser.parse input in
+    print_endline (Ast.to_json ast))
 ;;
