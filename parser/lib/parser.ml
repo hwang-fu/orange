0aaf8e41bff6baafede7c53c8300800dd20a9cb2
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-27 14:58:36 +0100

    feat: add Parser.create()

--- parser/lib/parser.ml
+++ parser/lib/parser.ml
@@ -2,3 +2,10 @@ type t =
   { lexer : Lexer.t
   ; mutable current : Token.t
   }
+
+(* Create parser from input string *)
+let create input =
+  let lexer = Lexer.create input in
+  let current = Lexer.next_token lexer in
+  { lexer; current }
+;;
