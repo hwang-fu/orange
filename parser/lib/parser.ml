4deda562f525fcb00755ee51f0522c45b83d1193
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-27 14:59:24 +0100

    feat: add Parser.advance()

--- parser/lib/parser.ml
+++ parser/lib/parser.ml
@@ -9,3 +9,6 @@ let create input =
   let current = Lexer.next_token lexer in
   { lexer; current }
 ;;
+
+(* Advance to next token *)
+let advance parser = parser.current <- Lexer.next_token parser.lexer
