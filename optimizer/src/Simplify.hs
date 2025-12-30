032e0a93856af08f0cf2a1987e803caf1cb32a4e
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-30 01:24:01 +0100

    feat: add simplify()

--- optimizer/src/Simplify.hs
+++ optimizer/src/Simplify.hs
@@ -1,5 +1,18 @@
 import Ast (Expr (..))
 
+-- | Apply simplification rules recursively
+-- Simplifies children first (bottom-up), then applies rules at current node
+simplify :: Expr -> Expr
+simplify expr =
+  let simplified = case expr of
+        -- Recursively simplify children first
+        And l r -> And (simplify l) (simplify r)
+        Or l r -> Or (simplify l) (simplify r)
+        Not e -> Not (simplify e)
+        -- Leaves stay as-is
+        other -> other
+   in applyRules simplified
+
 -- | Apply rewrite rules at current node
 applyRules :: Expr -> Expr
 applyRules expr = case expr of
