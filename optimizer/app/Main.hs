b1990335e7b53610a5db2c36088a25ea428a7f8f
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-30 01:33:26 +0100

    feat: wire up optimizer CLI with stdin/stdout pipeline

--- optimizer/app/Main.hs
+++ optimizer/app/Main.hs
@@ -6,6 +6,18 @@ import Emit (toJson)
 import JsonParser (parseExpr)
 import Lib (placeholder)
 import Simplify (simplify)
+import System.IO (hFlush, stdout)
 
 main :: IO ()
-main = putStrLn placeholder
+main = do
+  -- Read all input from stdin
+  input <- getContents
+
+  -- Parse, simplify, emit
+  case parseExpr input of
+    Left err -> do
+      putStrLn $ "Error: " ++ err
+    Right expr -> do
+      let optimized = simplify expr
+      putStrLn (toJson optimized)
+      hFlush stdout
