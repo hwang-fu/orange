3232f472cd06c48da46cdd974b31633dfcbbb30a
hwangfucius <junzhe.hwangfu@gmail.com>
2025-12-30 01:15:48 +0100

    feat: add expectRBrace()

--- optimizer/src/JsonParser.hs
+++ optimizer/src/JsonParser.hs
@@ -3,3 +3,9 @@ import JsonLexer (Token (..), lexJson)
 
 -- | Parse error description
 type ParseError = String
+
+-- | Expect closing brace
+expectRBrace :: [Token] -> Either ParseError [Token]
+expectRBrace (TokRBrace : rest) = Right rest
+expectRBrace (TokComma : TokRBrace : rest) = Right rest -- trailing comma ok
+expectRBrace _ = Left "Expected '}'"
